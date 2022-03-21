4501eaf4c264104ae5462abbedd55772236d5e8f
sheylz <farckovich@gmail.com>
2022-03-21 14:24:54 +0300

    updated upgrade mechanic

--- kega_cl.js
+++ kega_cl.js
@@ -1,5 +1,12 @@
+// TODO:
+// Сохранения (cookie)
+// Заводы кег (базовое производство)
+// Наполнение кег (продажа)
+// Реклама кег (повышение денег)
+
 var clicks = 1
 var clicks_per_upg = 1
+var upgrade_cost = 20
 document.getElementById('kega').setAttribute('draggable', false);
 
 function getCookie(name) {
@@ -23,9 +30,6 @@ function getCookie(name) {
 
 // clicks_per_upg = getCookie('clicks_per_upg')
 update_clicks()
-if (clicks >= 20) {
-    document.getElementById("upg_kega").style.visibility = "visible"; // Кнопка апгрейда кеги
-}
 
 function cl__() {
     clicks = +clicks + +clicks_per_upg
@@ -37,11 +41,29 @@ window.onbeforeunload = function() {
         document.cookie = "clicks_per_upg=" + clicks_per_upg
 }
 
+function getNoun(number, one, two, five) {
+    let n = Math.abs(number);
+    n %= 100;
+    if (n >= 5 && n <= 20) {
+      return five;
+    }
+    n %= 10;
+    if (n === 1) {
+      return one;
+    }
+    if (n >= 2 && n <= 4) {
+      return two;
+    }
+    return five;
+}
+
 function buy_upg() {
     if (clicks >= 20) {
         clicks_per_upg++
-        clicks -= 20
+        clicks -= upgrade_cost
         update_clicks()
+        upgrade_cost = Math.round(+upgrade_cost + +(upgrade_cost / 5))
+        document.getElementById("upg_kega").textContent = "Улучшить базовую Кегу (" + upgrade_cost + getNoun(upgrade_cost,' Кега', ' Кеги', ' Кег') + ")"
     }
 }
 
